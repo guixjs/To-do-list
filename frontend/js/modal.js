771eecfcc5676a78e5c38be4e751c1591f617e1a
Guilherme Felix <f3lixguilherme@gmail.com>
2025-01-06 20:53:49 -0300

    starting frontend

---
+++ frontend/js/modal.js
@@ -0,0 +1,21 @@
+const openButton = document.querySelectorAll(".buttonModal")
+const closeButton = document.querySelectorAll(".closeModal")
+
+openButton.forEach( button => {
+    button.addEventListener('click', () =>{
+        const modalId = button.getAttribute("data-modal")
+        const modal = document.getElementById(modalId)
+
+        modal.showModal()
+    })
+})
+
+
+closeButton.forEach( button => {
+    button.addEventListener('click', () =>{
+        const modalId = button.getAttribute("data-modal")
+        const modal = document.getElementById(modalId)
+
+        modal.close()
+    })
+})
